6c7671b069e1fa35af10e725e259173db62cee42
Wilson-A-Salamanca <wilsonsalamanca346@gmail.com>
2023-12-25 21:34:28 -0500

    add profile component and props title and handle

---
+++ pdas/src/ProfileCard.js
@@ -0,0 +1,12 @@
+function ProfileCard({title, handle}) {
+    //const {title, handle} = props;
+
+    return (
+        <div>
+            <h2>title is {title}</h2>
+            <p>Handle is {handle}</p>
+        </div>
+    );
+}
+
+export default ProfileCard;

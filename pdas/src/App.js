d6c5b15907edc78d42395910ef1f19059e41c07b
Wilson-A-Salamanca <wilsonsalamanca346@gmail.com>
2023-12-27 16:03:50 -0500

    adding image and content props and some styles

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,12 +1,45 @@
+import 'bulma/css/bulma.css'
 import ProfileCard from "./ProfileCard";
+import alexaImage from "./images/alexa.png";
+import cortanaImage from "./images/cortana.png";
+import siriImage from "./images/siri.png";
 
 function App() {
   return (
     <div>
-      <h1>Personal Digital Assistants</h1>
-      <ProfileCard title= "Alexa" handle = "@alexa99" />
-      <ProfileCard title= "Cortana" handle = "@cortana32"/>
-      <ProfileCard title= "Siri" handle = "@siri01"/>
+      <section className='hero is-primary'>
+        <div className='hero-body'>
+          <h1 className='title'>Personal Digital Assistants</h1>
+        </div>
+      </section>
+
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'>
+            <article className='column is-4'>
+              <ProfileCard 
+              title = "Alexa" 
+              handle = "@alexa99" 
+              image = {alexaImage} 
+              content = "Alexa was created by Amazon."/>
+            </article>
+            <article className='column is-4'>
+              <ProfileCard 
+              title = "Cortana" 
+              handle = "@cortana32" 
+              image = {cortanaImage}
+              content = "Cortana was made by Microsoft"/>
+            </article>
+            <article className='column is-4'>
+              <ProfileCard 
+              title = "Siri" 
+              handle = "@siri01" 
+              image = {siriImage}
+              content = "Siri was made by Apple"/>
+            </article>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,11 +1,21 @@
-function ProfileCard({title, handle}) {
+function ProfileCard({title, handle, image, content}) {
     //const {title, handle} = props;
 
     return (
-        <div>
-            <h2>title is {title}</h2>
-            <p>Handle is {handle}</p>
-        </div>
+        <section className = "card">
+            <article className = "card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="pda logo"/>
+                </figure>
+            </article>
+            <article className="card-content">
+                <article className="media-content">
+                    <h2 className="title is-4">{title}</h2>
+                    <p className="subtitle is-6">{handle}</p>
+                </article>
+                <article className="content">{content}</article>
+            </article>            
+        </section>
     );
 }
 
